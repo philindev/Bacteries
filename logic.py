75c4bceef570a74f8db63a650b202499496a8072
lib <lib@dvfu.ru>
2019-04-28 14:22:36 +1000

    logic

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 from bacterium import BacteriumOne, BacteruimTwo, BacteruimThree, BacteruimFour, BacteruimFive, ImprovedBacterium
+import bacterium
 from antibiotic import AntibioticStart, AntibioticMedium, AntibioticMaster
 
 
@@ -7,8 +8,8 @@ class Game:
         self.score = 0
         self.money = 100
         self.time = 0
-        self.army = {BacteriumOne: 0, BacteruimTwo: 0, BacteruimThree: 0,
-                     BacteruimFour: 0, BacteruimFive: 0, ImprovedBacterium: 0}
+        self.army = {'BacteriumOne': 0, 'BacteruimTwo': 0, 'BacteruimThree': 0,
+                     'BacteruimFour': 0, 'BacteruimFive': 0, 'ImprovedBacterium': 0}
         self.wave_count = 0
         self.achieved_waves = set()
 
@@ -25,16 +26,18 @@ class Game:
 
     def buy(self, bacteria):
         self.money -= bacteria().price
+        if self.money <= 0:
+            self.money = 0
+            return False
         self.army[bacteria] += 1
+        return bacteria()
 
     def killed(self, bacteria, array_bacterias):
         array_bacterias.remove(bacteria)
-        self.army[type(bacteria)] -= 1
+        self.army[bacteria.__class__.__name__] -= 1
 
     def alive(self, bacteria_array):
         for bact in bacteria_array:
             for wave in self.achieved_waves:
                 self.score += bact.score * wave.bonus
                 self.money += bact.cash * wave.bonus
-
-
